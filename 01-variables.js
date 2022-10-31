edd5e11df71385d587926d6474dfc0295c83cfe1
Praveen-Prasad <praveenprasad2510@gmail.com>
2022-10-31 08:38:06 +0530

    variable declaration

---
+++ 01-variables.js
@@ -0,0 +1,16 @@
+console.log ("new folder")
+var a=10
+a=30
+console.log(a)
+var name = "praveen" ;
+console.log(name)
+const hi="hello";
+console.log(hi)
+
+
+
+
+
+
+// var - redeclaration and reassigning is possible
+// let - only reassigning is possible 
