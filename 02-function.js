d08a8f33a349d5885f68c0a1720bb836e782a3d1
Praveen-Prasad <praveenprasad2510@gmail.com>
2022-11-07 07:31:50 +0530

    functions in js using parameter

--- 02-function.js
+++ 02-function.js
@@ -2,3 +2,16 @@ function printhello(){
     console.log("Hello")
 }
 printhello();
+
+function squareofTwo(){
+    console.log(2*2)
+}
+squareofTwo();
+
+function squareOf(x){
+        console.log(x*x)
+}
+squareOf(4);
+squareOf(5);
+squareOf(6);
+squareOf(7);
