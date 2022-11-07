00dab93a0eeabe10d12c5f21d493e0d96c4e1fdd
Praveen-Prasad <praveenprasad2510@gmail.com>
2022-11-07 09:10:01 +0530

    arrays in js

---
+++ 04-array.js
@@ -0,0 +1,19 @@
+let a=[1,2,3,4,5,6]
+a[2]=8
+console.log(a)
+
+a.push("hai")
+console.log(a)
+
+a.pop()
+console.log(a)
+
+a.unshift("hello")
+console.log(a)
+
+a.shift()
+console.log(a)
+
+let name=["p","r","a","v","e","e","n"]
+name = name.join("")
+console.log(name)
