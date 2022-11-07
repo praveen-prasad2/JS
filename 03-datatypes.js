05f0cc5fd49650ed5e5e5f47a0a4dece566b8396
Praveen-Prasad <praveenprasad2510@gmail.com>
2022-11-07 08:51:55 +0530

    Numbers and strings

---
+++ 03-datatypes.js
@@ -0,0 +1,27 @@
+let a=10
+a++
+console.log(a)
+
+let b=5
+b+=5
+console.log(b)
+
+let c=20%9
+console.log(c)
+
+console.log("hello "+"world")
+let msg="hello welcome "
+console.log(msg+msg)
+
+console.log(msg[9])
+
+let msge="hello world"
+console.log(msge[0]+msge[10])
+console.log(msge.length)
+
+let num=parseInt("12345");
+console.log(num)
+
+let str="praveen prasad"
+string=str.split(" ")
+console.log(string)
