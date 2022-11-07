819b707af9af86a2f3e0491093159a07922d3901
Praveen-Prasad <praveenprasad2510@gmail.com>
2022-11-07 08:12:51 +0530

    functions in js using parameter and argument

--- 02-function.js
+++ 02-function.js
@@ -15,3 +15,43 @@ squareOf(4);
 squareOf(5);
 squareOf(6);
 squareOf(7);
+
+function plusFour(x){
+        console.log(4+x)
+}
+plusFour(8);
+plusFour(5);
+plusFour(6);
+plusFour(7);
+
+function printSum(x,y){
+    console.log(x+y)
+}
+printSum(4,5);
+printSum(100,200);
+
+
+function convertToMinutes(x){
+    console.log(x*60)
+}
+convertToMinutes(1000);
+convertToMinutes(6);
+
+function convertToDays(x){
+    console.log(x*365)
+}
+convertToDays(5);
+convertToDays(4);
+
+function getFootballPoints(win,draw,lose){
+    
+    // console.log(3*win+1*draw)
+    return(3*win+1*draw)
+    console.log("Hello")
+}
+getFootballPoints(11,7,3);
+getFootballPoints(11,8,3);
+let a=getFootballPoints(6,6,6);
+console.log(a);
+
+console.log(getFootballPoints(6,6,6))
