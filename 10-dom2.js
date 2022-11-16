4b572e81b6b05aed94b385fabc10fd60f1f660db
Praveen-Prasad <praveenprasad2510@gmail.com>
2022-11-16 08:52:32 +0530

    Event Listners

---
+++ 10-dom2.js
@@ -0,0 +1,18 @@
+// let button=document.querySelector('button')
+// button.innerHTML="Click me"
+// console.log(button)
+
+let input=document.querySelector('#value')
+input.value="Hello"
+
+// let a=document.querySelector('.link')
+// a.innerHTML="click here"
+
+// let button=document.querySelectorAll('button')
+// button.innerHTML="clickme"
+// console.log(button)
+
+let button=document.querySelector('#btn')
+button.addEventListener("click",function(){
+    alert("Hello")
+})
